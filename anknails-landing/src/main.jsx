f45eafba4b9e0b6996893bd1138dc3f79f525877
paulmatafox <haqintosh@gmail.com>
2025-10-05 19:44:06 +0000

    Update main.jsx

--- anknails-landing/src/main.jsx
+++ anknails-landing/src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import AboutApp from "./AboutApp.jsx"; // 👈 додай цей імпорт
+import AboutApp from "./AboutApp.jsx"; // 👈 твоя сторінка "Про мене"
 import "./index.css";
 import "./i18n/i18n.js";
 
-// ✅ визначаємо режим (основний або "about")
-const mode = import.meta.env.VITE_PAGE_MODE || "main";
+const mode = import.meta.env.VITE_PAGE_MODE;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
