dfd4d96f747d07da935863cfa27c31e4ec00ce3b
paulmatafox <haqintosh@gmail.com>
2025-10-05 10:50:57 +0200

    Update tailwind.config.js

--- anknails-landing/tailwind.config.js
+++ anknails-landing/tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "media",
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
         blush: "#fbe0e6",
         rose: "#f6b6c5",
-        night: "#1a1a1a",
       },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
