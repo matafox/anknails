e6f6396f23028f79a80826703aa4b38e8a5c1a3e
paulmatafox <haqintosh@gmail.com>
2025-10-23 17:07:29 +0200

    Update LoginPage.jsx

--- anknails-landing/src/pages/LoginPage.jsx
+++ anknails-landing/src/pages/LoginPage.jsx
@@ -36,7 +36,7 @@ export default function LoginPage() {
       const res = await fetch("https://anknails-backend-production.up.railway.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email, password, lang: i18n.language, }),
       });
 
       const data = await res.json();
