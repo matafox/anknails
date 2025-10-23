16d46348d80f95f0fe37a2a5cc89ffc3361c73e1
paulmatafox <haqintosh@gmail.com>
2025-10-23 14:48:30 +0200

    Update app.py

--- anknails-landing/backend/app.py
+++ anknails-landing/backend/app.py
@@ -121,4 +121,4 @@ def root():
 # ============================================================
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("backend.app:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
