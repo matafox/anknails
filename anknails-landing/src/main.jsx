132572d9da549614eaa0c55296b3ccb34eecd0ff
paulmatafox <haqintosh@gmail.com>
2025-10-23 15:58:40 +0200

    Update main.jsx

--- anknails-landing/src/main.jsx
+++ anknails-landing/src/main.jsx
@@ -6,6 +6,7 @@ import AboutApp from "./AboutApp.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import LoginPage from "./pages/LoginPage.jsx"; 
 import AdminPage from "./pages/AdminPage.jsx";
+import CabinetPage from "./pages/CabinetPage.jsx";
 
 import "./index.css";
 import "./i18n/i18n.js";
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/login" element={<LoginPage />} /> 
         <Route path="*" element={<NotFound />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/cabinet" element={<CabinetPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
