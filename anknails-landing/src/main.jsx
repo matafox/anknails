0f11ac86f578a2a9126acb516d634b2e01d2a32d
paulmatafox <haqintosh@gmail.com>
2025-11-18 14:26:20 +0100

    Update main.jsx

--- anknails-landing/src/main.jsx
+++ anknails-landing/src/main.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App.jsx";
 import AboutApp from "./AboutApp.jsx";
 import NotFound from "./pages/NotFound.jsx";
-import LoginPage from "./pages/LoginPage.jsx"; 
+import LoginPage from "./pages/LoginPage.jsx";
 import AdminPage from "./pages/AdminPage.jsx";
 import CabinetPage from "./pages/CabinetPage.jsx";
 
 import "./index.css";
 import "./i18n/i18n.js";
 
+const basename = import.meta.env.VITE_BASE_PATH || "/";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<AboutApp />} />
-        <Route path="/login" element={<LoginPage />} /> 
-        <Route path="*" element={<NotFound />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/profile" element={<CabinetPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
