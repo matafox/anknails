42075abcb7b90eabc14fd7e7342ea524bba7e23f
paulmatafox <haqintosh@gmail.com>
2025-11-19 08:59:31 +0100

    Update main.jsx

--- anknails-landing/src/main.jsx
+++ anknails-landing/src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import AboutApp from "./AboutApp.jsx";
 import NotFound from "./pages/NotFound.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx"; 
 import AdminPage from "./pages/AdminPage.jsx";
 import CabinetPage from "./pages/CabinetPage.jsx";
 
@@ -14,21 +15,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* редірект з кореня на глобальний логін */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-
-        {/* 🌍 глобальні маршрути без платформи */}
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<AboutApp />} />
+        <Route path="/login" element={<LoginPage />} /> 
+        <Route path="*" element={<NotFound />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/profile" element={<CabinetPage />} />
-
-        {/* 🧩 ті самі сторінки, але з будь-яким slug платформи */}
-        <Route path="/:platformSlug/login" element={<LoginPage />} />
-        <Route path="/:platformSlug/admin" element={<AdminPage />} />
-        <Route path="/:platformSlug/profile" element={<CabinetPage />} />
-
-        {/* 404 */}
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
